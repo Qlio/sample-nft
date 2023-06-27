bfb9d83b81107a8f98ebc52d68c7f54f36d0e76a
Bulgantamir Gankhuyag <bulgaadev@gmail.com>
2023-06-27 15:37:28 +0800

    baks json

--- generate-jsons.js
+++ generate-jsons.js
@@ -2,19 +2,35 @@ const axios = require("axios");
 const fs = require("fs");
 
 const main = async () => {
-    for (let i = 9616; i < 10000; i++) {
-        console.log(i);
-        const { data } = await axios.get(
-            `https://boredapeyachtclub.com/api/mutants/${i}`
-        );
-        fs.writeFileSync(
-            `./jsons-mayc/${i}.json`,
-            JSON.stringify({
-                image: `https://raw.githubusercontent.com/Qlio/sample-nft/main/images-mayc/${i}.png`,
-                attributes: data.attributes,
-            })
-        );
-    }
+    let continuation;
+    do {
+        const {
+            data: { tokens, continuation: c },
+        } = await axios.get("https://api.reservoir.tools/tokens/v6", {
+            params: {
+                collection: "0xba30e5f9bb24caa003e9f2f0497ad287fdf95623",
+                sortBy: "tokenId",
+                sortDirection: "asc",
+                includeAttributes: true,
+                limit: 100,
+                continuation: continuation,
+            },
+        });
+        continuation = c;
+
+        for (const { token } of tokens) {
+            fs.writeFileSync(
+                `./jsons-bakc/${token.tokenId}.json`,
+                JSON.stringify({
+                    image: `https://raw.githubusercontent.com/Qlio/sample-nft/main/images-bakc/${token.tokenId}.png`,
+                    attributes: token.attributes.map(({ key, value }) => ({
+                        trait_type: key,
+                        value,
+                    })),
+                })
+            );
+        }
+    } while (continuation);
 };
 
 main();
